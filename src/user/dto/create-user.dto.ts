bf116711453da9941e08c71e7b5d2dffc0b62a7d
Andrey Emelianov <621077@mail.ru>
2023-09-26 21:16:00 +0300

    подключил class-transformer перенёс декоратор isEmail для валидации email из entity в dto, так как до этого он не отрабатывал

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail } from 'class-validator';
+
 export class CreateUserDto {
   fullName: string;
+
+  @IsEmail(undefined, { message: 'Неверная почта' })
   email: string;
+
   password?: string;
 }
