5315c857b593fdf6d0d2b8fc659ecd289ef692b9
Andrey Emelianov <621077@mail.ru>
2023-09-26 21:23:10 +0300

    добавил декораторы для валидации длинны пароля и имени

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,10 +1,12 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, Length } from 'class-validator';
 
 export class CreateUserDto {
+  @Length(3)
   fullName: string;
 
   @IsEmail(undefined, { message: 'Неверная почта' })
   email: string;
 
+  @Length(6, 32, { message: 'Пароль должен быть минимум 6 сиволов' })
   password?: string;
 }
